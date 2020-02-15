3ed6114a12d45de0ca0cc1ee3c960a3d93180bc3
plumpmango <clemence-cb@hotmail.fr>
2020-02-15 12:06:49 +0100

    cas1,2,3,rapport

--- projet/appariemment.py
+++ projet/appariemment.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+import math
+import os
 
 def eqm(bloc1,bloc2):
     l,h,c = bloc1.shape
@@ -8,7 +10,8 @@ def eqm(bloc1,bloc2):
     EQM = e / (l*h*c)
     return EQM
 
-def calculMeilleurBloc(windowIm1,bloc2):
+#Recherche exhaustive
+def calculMeilleurBloc1(windowIm1,bloc2):
     errqm = float("inf")
     lig,col,c1 = windowIm1.shape
     ligBloc,colBloc,c2 = bloc2.shape
@@ -23,7 +26,7 @@ def calculMeilleurBloc(windowIm1,bloc2):
                 blocf = bloctmp
     return blocf
 
-def rechercheExhaustive(im1,im2,window):
+def reconstruction(im1,im2,window,N):
     #parcours par fenetre 16*16
     t = window/2
     l,h,c = im1.shape
@@ -33,29 +36,92 @@ def rechercheExhaustive(im1,im2,window):
             # blocIm2 = [i-4:i+4][j-4:j+4]
             voisinIm1 = im1[i-t:i+t,j-t:j+t,:]
             blocIm2 = im2[i-4:i+4,j-4:j+4,:]
-            im2C[i-4:i+4,j-4:j+4,:] = calculMeilleurBloc(voisinIm1,blocIm2)
+            im2C[i-4:i+4,j-4:j+4,:] = globals()['calculMeilleurBloc' + str(N)](voisinIm1,blocIm2)
             # print(str(i) + "," +str(j)+"\n")
 
     return im2C
 
-def calculerH(dx,dy,dxdy,i,j):
-    a = dx[i-1:i+1,j-1:j+1]
-    A = sum(a)
-    B = sum(dy[i-1:i+1,j-1:j+1])
-    C = sum(dxdy[i-1:i+1,j-1:j+1])
+#Recherche log2
+def calculMeilleurBloc2(window,block):
+    errqm = float("inf")
+    windowSize = len(window)
+    blockSize = len(block)
+    midX = windowSize/2
+    midY = windowSize/2
+    pas = windowSize/4
+    while pas >= 1:
 
-    H = np.array([[A, B]; [B, C]])
+        xL = midX-blockSize/2
+        xR = midX+blockSize/2
+        indexLineLeft =  [xL,xL,xL-pas,xL+pas,xL]
+        indexLineRight = [xR,xR,xR-pas,xR+pas,xR]
+
+        yL = midY-blockSize/2
+        yR = midY+blockSize/2
+        indexColumnLeft = [yL-pas,yL+pas,yL,yL,yL]
+        indexColumRight = [yR-pas,yR+pas,yR,yR,yR]
+
+        for i in range(0,len(indexLineLeft)):
+            if (indexLineLeft[i] < 0) or (indexColumnLeft[i] < 0) or (indexLineRight[i] >= windowSize) or (indexColumRight[i] >= windowSize):
+                continue
+
+            temp = window[indexLineLeft[i]:indexLineRight[i],indexColumnLeft[i]:indexColumRight[i],:]
+            tempEqm =  eqm(temp,block)
+
+			#bloc gauche
+            if tempEqm < errqm and i == 0:
+                errqm = tempEqm
+                res = temp
+                midY = midY-pas
+                break
+
+			#bloc droite
+            if tempEqm < errqm and i ==1:
+                errqm = tempEqm
+                res = temp
+                midY=midY+pas
+                break
+
+			#bloc haut
+            if tempEqm < errqm and i == 2:
+                errqm = tempEqm
+                res = temp
+                midX = midX-pas
+                break
+
+			#bloc bas
+            if tempEqm < errqm and i == 3:
+                errqm = tempEqm
+                res = temp
+                midX = midX+pas
+                break
+
+			#bloc mid
+            if tempEqm <= errqm and i == 4:
+                errqm = tempEqm
+                res = temp
+                pas = pas/2
+                break
+    return(res)
+
+#LUKas et Kanade
+def calculerH(dx,dy,dxdy,i,j):
+    A = sum(sum(sum(dx[i-1:i+1,j-1:j+1])))
+    B = sum(sum(sum(dy[i-1:i+1,j-1:j+1])))
+    C = sum(sum(sum(dxdy[i-1:i+1,j-1:j+1])))
+
+    H = np.array([[A, B], [B, C]])
 
     return H
 
 def calculerb(dxdt,dydt,i,j):
-    bx = sum(dxdt[i-1:i+1,j-1:j+1])
-    by = sum(dydt[i-1:i+1,j-1:j+1])
+    bx = sum(sum(sum(dxdt[i-1:i+1,j-1:j+1])))
+    by = sum(sum(sum(dydt[i-1:i+1,j-1:j+1])))
 
-    b = np.array([bx; by])
+    b = np.array([bx, by])
     return b
 
-def lucasKanade(im1,Iim2):
+def lukasKanade(im1,im2):
     l,h,c = im1.shape
 
     # Gradient
@@ -73,16 +139,50 @@ def lucasKanade(im1,Iim2):
     dx = np.power(dx,2)
     dy = np.power(dy,2)
 
-    xx,dxdy = np.gradient(dx)
+    dxdy = np.gradient(dx)[1]
 
-    imS = np.zeros(l,h,2,1)
+    imS = np.zeros((l,h,2,))
 
-    for i in range(2,l):
-        for j in range(2,h):
+    for i in range(1,l-8,8):
+        for j in range(1,h-8,8):
             H = calculerH(dx,dy,dxdy,i,j)
             b = calculerb(dxdt,dydt,i,j)
-            vp,vectp = abs(np.linalg.eig(H))
-            v = np.linalg.lstsq(H,b)
-            imS[i,j,:,:] = v
+            vp = np.linalg.eig(H)[0]
+            if (vp[0] > 0.1) and (vp[1] > 0.1) :
+                v = abs(np.linalg.lstsq(H,b)[0])
+            else:
+                v = np.zeros((2,))
+            imS[i,j,:] = v
 
     return imS
+
+def calculerIm(im1,im2,deltaIm):
+    l,h,c = im1.shape
+    # print im1.shape
+    # newIm = np.zeros((l,h,c))
+    newIm = im1
+    for i in range(1,l-8,8):
+        for j in range(1,h-8,8):
+            # print type(deltaIm[0])
+            indX = abs(int(math.floor(i-deltaIm[i,j,0])))
+            indY = abs(int(math.floor(j-deltaIm[i,j,1])))
+
+            if indX > l :
+                indX = l-1
+            if indX < 1 :
+                indX = 1
+
+            if indY < 1 :
+                indY = 1
+            if indY > h :
+                indY = h-1
+
+            newIm[i-1:i+1,j-1:j+1,:] = im2[indX-1:indX+1,indY-1:indY+1,:]
+
+    return newIm
+
+def casLK(img1,img2):
+    deltaIm = lukasKanade(img1,img2);
+    # print deltaIm.shape
+    newIm = calculerIm(img2,img1,deltaIm)
+    return newIm
--- projet/main.py
+++ projet/main.py
@@ -6,13 +6,6 @@ import appariemment as ap
 import matplotlib.pyplot as plt
 
 
-def ecrireImageFichier(img,nomFic):
-    l,h = img.shape
-    fichier = open(nomFic,"a")
-    for i in range(0,l):
-        for j in range(0,h):
-            fichier.write(str(img2[i][j]) + " ")
-        fichier.write("\n")
 
 kaamelott1 = cv.imread('./images/kaamelott1.jpeg')
 kaamelott2 = cv.imread('./images/kaamelott2.jpeg')
@@ -27,22 +20,101 @@ desert2 = cv.imread('./images/desert2.jpeg')
 friends1 = cv.imread('./images/friends1.jpeg')
 friends2 = cv.imread('./images/friends2.jpeg')
 
-westworld1 = cv.imread('./images/kaa1.jpeg')
-westworld2 = cv.imread('./images/kaa2.jpeg')
+westworld1 = cv.imread('./images/westworld1.jpeg')
+westworld2 = cv.imread('./images/westworld2.jpeg')
+
+
+#Recherche exhaustive
+
+window = 16
+
+result = ap.reconstruction(kaamelott1,kaamelott2,window,1)
+cv.imwrite('./result.jpeg',result)
+
+result2 = ap.reconstruction(kaa1,kaa2,window,1)
+cv.imwrite('./result2.jpeg',result2)
+
+result3 = ap.reconstruction(desert,desert2,window,1)
+cv.imwrite('./result3.jpeg',result3)
+
+resultfriends = ap.reconstruction(friends1,friends2,window,1)
+cv.imwrite('./resultfriends.jpeg',resultfriends)
+
+resultwestworld = ap.reconstruction(westworld1,westworld2,window,1)
+cv.imwrite('./resultwestworld.jpeg',resultwestworld)
 
 result = cv.imread('./result.jpeg')
 result2 = cv.imread('./result2.jpeg')
 result3 = cv.imread('./result3.jpeg')
 resultfriends = cv.imread('./resultfriends.jpeg')
-resultwestword = cv.imread('./resultwestworld.jpeg')
+resultwestworld = cv.imread('./resultwestworld.jpeg')
 
-window = 16
-# result = ap.rechercheExhaustive(img1,img2,window)
-# ecrireImageFichier(result,"result.txt")
-# print "over"
-# cv.imwrite('./resultwestworld.jpeg',result)
-# cv.waitKey(20000)
+print "Erreur quadratique moyenne kaamelott 1 : " + str(ap.eqm(kaamelott2,result))
+print "Erreur quadratique moyenne kaamelott 2 : " + str(ap.eqm(kaa2,result2))
+print "Erreur quadratique moyenne kaamelott 3 : " + str(ap.eqm(desert2,result3))
+print "Erreur quadratique moyenne friends : " + str(ap.eqm(friends2,resultfriends))
+print "Erreur quadratique moyenne westworld : " + str(ap.eqm(westworld2,resultwestworld))
 
-print "Erreur quadratique moyenne kaamelott 1 : " + str(ap.eqm(friends1,resultfriends))
 
-# cv.destroyAllWindows()
+
+#Recherche en log2
+resultLog2 = ap.reconstruction(kaamelott1,kaamelott2,16,2)
+cv.imwrite('./resultLog2.jpeg',resultLog2)
+
+result2Log2 = ap.reconstruction(kaa1,kaa2,16,2)
+cv.imwrite('./result2Log2.jpeg',result2Log2)
+
+result3Log2 = ap.reconstruction(desert,desert2,16,2)
+cv.imwrite('./result3Log2.jpeg',result3Log2)
+
+resultfriendsLog2 = ap.reconstruction(friends1,friends2,16,2)
+cv.imwrite('./resultfriendsLog2.jpeg',resultfriendsLog2)
+
+
+resultwestworldLog2 = ap.reconstruction(westworld1,westworld2,16,2)
+cv.imwrite('./resultwestworldLog2.jpeg',resultwestworldLog2)
+
+
+resultLog2 = cv.imread('./resultLog2.jpeg')
+result2Log2 = cv.imread('./result2Log2.jpeg')
+result3Log2 = cv.imread('./result3Log2.jpeg')
+resultfriendsLog2 = cv.imread('./resultfriendsLog2.jpeg')
+resultwestworldLog2 = cv.imread('./resultwestworldLog2.jpeg')
+
+print "Erreur quadratique moyenne kaamelott 1 : " + str(ap.eqm(kaamelott2,resultLog2))
+print "Erreur quadratique moyenne kaamelott 2 : " + str(ap.eqm(kaa2,result2Log2))
+print "Erreur quadratique moyenne kaamelott 3 : " + str(ap.eqm(desert2,result3Log2))
+print "Erreur quadratique moyenne friends : " + str(ap.eqm(friends2,resultfriendsLog2))
+print "Erreur quadratique moyenne westworld : " + str(ap.eqm(westworld2,resultwestworldLog2))
+
+
+#Lukas et kanade
+resultLK = ap.casLK(kaamelott1,kaamelott2)
+cv.imwrite('./resultLKv2.jpeg',resultLK)
+
+resultLK2 = ap.casLK(kaa1,kaa2)
+cv.imwrite('./resultLK2v2.jpeg',resultLK2)
+
+resultLK3 = ap.casLK(desert,desert2)
+cv.imwrite('./resultLK3v2.jpeg',resultLK3)
+
+resultLKfriends = ap.casLK(friends1,friends2)
+cv.imwrite('./resultLKfriendsv2.jpeg',resultLKfriends)
+
+resultLKwestworld = ap.casLK(westworld1,westworld2)
+cv.imwrite('./resultLKwestworldv2.jpeg',resultLKwestworld)
+
+
+
+# resultLK = cv.imread('./resultLKv2.jpeg')
+# resultLK2 = cv.imread('./resultLK2v2.jpeg')
+# resultLK3 = cv.imread('./resultLK3v2.jpeg')
+# resultLKfriends = cv.imread('./resultLKfriendsv2.jpeg')
+# resultLKwestworld = cv.imread('./resultLKwestworldv2.jpeg')
+
+
+print "Erreur quadratique moyenne kaamelott 1 : " + str(ap.eqm(kaamelott2,resultLK))
+print "Erreur quadratique moyenne kaamelott 2 : " + str(ap.eqm(kaa2,resultLK2))
+print "Erreur quadratique moyenne kaamelott 3 : " + str(ap.eqm(desert2,resultLK3))
+print "Erreur quadratique moyenne friends : " + str(ap.eqm(friends2,resultLKfriends))
+print "Erreur quadratique moyenne westworld : " + str(ap.eqm(westworld2,resultLKwestworld))
